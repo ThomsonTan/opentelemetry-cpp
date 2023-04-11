c4e619f32e65697a0d6f561411b791cd30e4c691
Tom Tan <Tom.Tan@microsoft.com>
2023-04-11 14:45:35 -0700

    Support multi-threaded benchmark

--- api/test/logs/logger_benchmark.cc
+++ api/test/logs/logger_benchmark.cc
@@ -9,7 +9,12 @@
 #  include "opentelemetry/logs/provider.h"
 #  include "opentelemetry/nostd/shared_ptr.h"
 
-#include <benchmark/benchmark.h>
+#  include <condition_variable>
+#  include <thread>
+#  include <mutex>
+#  include <vector>
+
+#  include <benchmark/benchmark.h>
 
 using opentelemetry::logs::EventId;
 using opentelemetry::logs::Logger;
@@ -28,13 +33,87 @@ namespace log_api = opentelemetry::logs;
 namespace
 {
 
+class Barrier {
+public:
+    explicit Barrier(std::size_t iCount) : 
+      mThreshold(iCount), 
+      mCount(iCount), 
+      mGeneration(0) {
+    }
+
+    void Wait() {
+        std::unique_lock<std::mutex> lLock{mMutex};
+        auto lGen = mGeneration;
+        if (!--mCount) {
+            mGeneration++;
+            mCount = mThreshold;
+            mCond.notify_all();
+        } else {
+            mCond.wait(lLock, [this, lGen] { return lGen != mGeneration; });
+        }
+    }
+
+private:
+    std::mutex mMutex;
+    std::condition_variable mCond;
+    std::size_t mThreshold;
+    std::size_t mCount;
+    std::size_t mGeneration;
+};
+
+static void ThreadRoutine(Barrier &barrier, benchmark::State &state, int thread_id, std::function<void(benchmark::State &state)> func)
+{
+    barrier.Wait();
+
+    if (thread_id == 0) {
+        state.ResumeTiming();
+    }
+
+    barrier.Wait();
+
+    for (int64_t i = 0; i < 0x100000000ull; i++) {
+        func(state);
+    }
+
+    if (thread_id == 0) {
+        state.PauseTiming();
+    }
+
+    barrier.Wait();
+}
+
+void MultiThreadRunner(benchmark::State& state, std::function<void(benchmark::State &state)> func)
+{
+    int num_threads = std::thread::hardware_concurrency();
+
+    Barrier barrier(num_threads);
+
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < num_threads; i++) {
+        threads.emplace_back(ThreadRoutine, std::ref(barrier), std::ref(state), i, func);
+    }
+
+    for (auto &thread : threads) {
+        thread.join();
+    }
+}
+
+
 static void BM_LoggerSingleCall_UnstructuredLog(benchmark::State &state)
 {
     auto lp = Provider::GetLoggerProvider();
     auto logger = lp->GetLogger("UnstructuredLog");
 
     for (auto _ : state) {
-        logger->Trace("This is a simple unstructured log message");
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [&logger](benchmark::State &state) {
+            logger->Trace("This is a simple unstructured log message");
+            return 0;
+        });
+
+        state.ResumeTiming();
     }
 }
 BENCHMARK(BM_LoggerSingleCall_UnstructuredLog);
@@ -45,8 +124,15 @@ static void BM_LoggerSingleCall_StructuredLog(benchmark::State &state)
     auto logger = lp->GetLogger("StructuredLog");
 
     for (auto _ : state) {
-        logger->Trace("This is a simple structured log message from {process_id}:{thread_id}",
-                      opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [&logger](benchmark::State &state) {
+            logger->Trace("This is a simple structured log message from {process_id}:{thread_id}",
+                        opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+            return 0;
+        });
+
+        state.ResumeTiming();
     }
 }
 BENCHMARK(BM_LoggerSingleCall_StructuredLog);
@@ -57,8 +143,17 @@ static void BM_LoggerSingleCall_StructuredLogWithEventId(benchmark::State &state
     auto logger = lp->GetLogger("StructuredLogWithEventId");
 
     for (auto _ : state) {
-        logger->Trace(0x1234567890, "This is a simple structured log message from {process_id}:{thread_id}",
-                      opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [&logger](benchmark::State &state) {
+            logger->Trace("This is a simple structured log message from {process_id}:{thread_id}",
+                        opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+            logger->Trace(0x1234567890, "This is a simple structured log message from {process_id}:{thread_id}",
+                        opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+            return 0;
+        });
+
+        state.ResumeTiming();
     }
 }
 BENCHMARK(BM_LoggerSingleCall_StructuredLogWithEventId);
@@ -71,8 +166,17 @@ static void BM_LoggerSingleCall_StructuredLogWithEventIdStruct(benchmark::State 
     const EventId function_name_event_id{0x12345678, "Company.Component.SubComponent.FunctionName"};
 
     for (auto _ : state) {
-        logger->Trace(function_name_event_id, "Simulate function enter trace message from {process_id}:{thread_id}",
-                      opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [&logger, &function_name_event_id](benchmark::State &state) {
+            logger->Trace(function_name_event_id, "Simulate function enter trace message from {process_id}:{thread_id}",
+                        opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+            logger->Trace(function_name_event_id, "Simulate function enter trace message from {process_id}:{thread_id}",
+                        opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
+            return 0;
+        });
+
+        state.ResumeTiming();
     }
 }
 BENCHMARK(BM_LoggerSingleCall_StructuredLogWithEventIdStruct);
@@ -89,15 +193,15 @@ class LoggerFixture : public benchmark::Fixture
     void TearDown(const ::benchmark::State &state) {}
 
     // test methods
-    int FunctionWithUnstructuredLog();
+    int FunctionWithUnstructuredLog(benchmark::State &state);
 
-    int FunctionWithStructuredLog();
+    int FunctionWithStructuredLog(benchmark::State &state);
 
-    int FunctionWithStructuredLogAndEventId();
+    int FunctionWithStructuredLogAndEventId(benchmark::State &state);
 
-    int FunctionWithStructuredLogAndEventIdStruct();
+    int FunctionWithStructuredLogAndEventIdStruct(benchmark::State &state);
 
-    int FunctionWithStructuredLogAndEventIdStructCheckEnabled();
+    int FunctionWithStructuredLogAndEventIdStructCheckEnabled(benchmark::State &state);
 
 private:
     nostd::shared_ptr<log_api::Logger> logger;
@@ -105,7 +209,7 @@ private:
     const EventId function_exit_event_id{0x12345679, "Company.Component.SubComponent.FunctionExit"};
 };
 
-int LoggerFixture::FunctionWithUnstructuredLog()
+int LoggerFixture::FunctionWithUnstructuredLog(benchmark::State &state)
 {
     logger->Trace("This is a simple unstructured log message");
 
@@ -113,7 +217,7 @@ int LoggerFixture::FunctionWithUnstructuredLog()
     return 0;
 }
 
-int LoggerFixture::FunctionWithStructuredLog()
+int LoggerFixture::FunctionWithStructuredLog(benchmark::State &state)
 {
     logger->Trace("This is a simple structured log message from {process_id}:{thread_id}",
                   opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
@@ -123,7 +227,7 @@ int LoggerFixture::FunctionWithStructuredLog()
     return 0;
 }
 
-int LoggerFixture::FunctionWithStructuredLogAndEventId()
+int LoggerFixture::FunctionWithStructuredLogAndEventId(benchmark::State &state)
 {
     logger->Trace(0x1234567890, "This is a simple structured log message from {process_id}:{thread_id}",
                   opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
@@ -134,7 +238,7 @@ int LoggerFixture::FunctionWithStructuredLogAndEventId()
     return 0;
 }
 
-int LoggerFixture::FunctionWithStructuredLogAndEventIdStruct()
+int LoggerFixture::FunctionWithStructuredLogAndEventIdStruct(benchmark::State &state)
 {
   logger->Trace(function_enter_event_id, "Simulate function enter trace message from {process_id}:{thread_id}",
                 opentelemetry::common::MakeAttributes({{"process_id", 12347}, {"thread_id", 12348}}));
@@ -144,7 +248,7 @@ int LoggerFixture::FunctionWithStructuredLogAndEventIdStruct()
   return 0;
 }
 
-int LoggerFixture::FunctionWithStructuredLogAndEventIdStructCheckEnabled()
+int LoggerFixture::FunctionWithStructuredLogAndEventIdStructCheckEnabled(benchmark::State &state)
 {
   if (logger->Enabled(Severity::kTrace, function_enter_event_id)) {
       logger->Trace(function_enter_event_id, "Simulate function enter trace message from {process_id}:{thread_id}",
@@ -162,7 +266,13 @@ int LoggerFixture::FunctionWithStructuredLogAndEventIdStructCheckEnabled()
 BENCHMARK_DEFINE_F(LoggerFixture, BM_LoggerFunctionWithUnstructuredLog)(benchmark::State &state)
 {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(FunctionWithUnstructuredLog());
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [this](benchmark::State &state){
+            FunctionWithUnstructuredLog(state);
+        });
+
+        state.ResumeTiming();
     }
 }
 
@@ -171,7 +281,13 @@ BENCHMARK_REGISTER_F(LoggerFixture, BM_LoggerFunctionWithUnstructuredLog);
 BENCHMARK_DEFINE_F(LoggerFixture, BM_LoggerFunctionWithStructuredLog)(benchmark::State &state)
 {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(FunctionWithStructuredLog());
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [this](benchmark::State &state){
+            FunctionWithStructuredLog(state);
+        });
+
+        state.ResumeTiming();
     }
 }
 
@@ -180,7 +296,13 @@ BENCHMARK_REGISTER_F(LoggerFixture, BM_LoggerFunctionWithStructuredLog);
 BENCHMARK_DEFINE_F(LoggerFixture, BM_LoggerFunctionWithStructuredLogAndEventId)(benchmark::State &state)
 {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(FunctionWithStructuredLogAndEventId());
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [this](benchmark::State &state){
+            FunctionWithStructuredLogAndEventId(state);
+        });
+
+        state.ResumeTiming();
     }
 }
 
@@ -189,7 +311,13 @@ BENCHMARK_REGISTER_F(LoggerFixture, BM_LoggerFunctionWithStructuredLogAndEventId
 BENCHMARK_DEFINE_F(LoggerFixture, BM_LoggerFunctionWithStructuredLogAndEventIdStruct)(benchmark::State &state)
 {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(FunctionWithStructuredLogAndEventIdStruct());
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [this](benchmark::State &state){
+            FunctionWithStructuredLogAndEventIdStruct(state);
+        });
+
+        state.ResumeTiming();
     }
 }
 
@@ -198,7 +326,13 @@ BENCHMARK_REGISTER_F(LoggerFixture, BM_LoggerFunctionWithStructuredLogAndEventId
 BENCHMARK_DEFINE_F(LoggerFixture, BM_LoggerFunctionWithStructuredLogAndEventIdStructCheckEnabled)(benchmark::State &state)
 {
     for (auto _ : state) {
-        benchmark::DoNotOptimize(FunctionWithStructuredLogAndEventIdStructCheckEnabled());
+        state.PauseTiming();
+
+        MultiThreadRunner(state, [this](benchmark::State &state){
+            FunctionWithStructuredLogAndEventIdStructCheckEnabled(state);
+        });
+
+        state.ResumeTiming();
     }
 }
 
